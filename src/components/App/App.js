1fba13528eddec2b9732756f723b311a444515fc
Darya Kasyanova <daskas007@gmail.com>
2024-01-23 16:10:18 +0700

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+
+import MovieList from '../MovieList/MovieList';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='movie'>
+      <MovieList />
+    </div>
+  );
+};
+
+export default App;
